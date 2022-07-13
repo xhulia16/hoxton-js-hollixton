e3dbe8e10c57551fecc5251b3482c99a38b08a6d
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-07-13 21:42:57 +0200

    display details

--- src/main.ts
+++ src/main.ts
@@ -1,148 +1,190 @@
 import './style.css'
 
-type Item={
-id: number, 
-type: string,
-name: string,
-image: string,
-price: number,
-discountedPrice: number| null,
-dateEntered: string,
-stock: number
+type Item = {
+    id: number,
+    type: string,
+    name: string,
+    image: string,
+    price: number,
+    discountedPrice: number | null,
+    dateEntered: string,
+    stock: number
 }
-type State={
-items: Item[]
+type State = {
+    items: Item[],
+    selectedItem:Item[]
 }
-let state: State={
-    items:[]
+let state: State = {
+    items: [],
+    selectedItem: []
 }
 
-function getStoreItems(){
-fetch('http://localhost:3005/store')
-.then(resp => resp.json())
-.then(dataFromServer =>{
-    state.items=dataFromServer
-    renderMain()
-})
+function getStoreItems() {
+    fetch('http://localhost:3005/store')
+        .then(resp => resp.json())
+        .then(dataFromServer => {
+            state.items = dataFromServer
+            renderMain()
+        })
 }
 
 console.log(state)
 
-function renderHeader(){
- //   <div class="left-header">
- //       <h1 class="title">HOLLIXTON</h1>
- //       <nav>
- //         <ul>
- //           <li>Girls</li>
- //           <li>Guys</li>
- //           <li>Sale</li>
- //         </ul>
- //       </nav>
- //     </div>
-  //     <div class="right-header">
- //       <nav>
- //         <ul>
- //           <li>🔍</li>
- //           <li>👤</li>
- //           <li>🛍️</li>
- //         </ul>
- //       </nav>
- //     </div>
+function renderHeader() {
+    //   <div class="left-header">
+    //       <h1 class="title">HOLLIXTON</h1>
+    //       <nav>
+    //         <ul>
+    //           <li>Girls</li>
+    //           <li>Guys</li>
+    //           <li>Sale</li>
+    //         </ul>
+    //       </nav>
+    //     </div>
+    //     <div class="right-header">
+    //       <nav>
+    //         <ul>
+    //           <li>🔍</li>
+    //           <li>👤</li>
+    //           <li>🛍️</li>
+    //         </ul>
+    //       </nav>
+    //     </div>
 
- let headerEl=document.querySelector('header')
- if (headerEl === null) return
- headerEl.textContent=''
+    let headerEl = document.querySelector('header')
+    if (headerEl === null) return
+    headerEl.textContent = ''
 
- let leftHeaderEl=document.createElement('div')
- leftHeaderEl.className='left-header'
+    let leftHeaderEl = document.createElement('div')
+    leftHeaderEl.className = 'left-header'
 
- let websiteTitleEl=document.createElement('h1')
- websiteTitleEl.className='title'
- websiteTitleEl.textContent='HOLLIXTON'
+    let websiteTitleEl = document.createElement('h1')
+    websiteTitleEl.className = 'title'
+    websiteTitleEl.textContent = 'HOLLIXTON'
 
- let leftNavEl=document.createElement('nav')
- let leftUlEl=document.createElement('ul')
+    let leftNavEl = document.createElement('nav')
+    let leftUlEl = document.createElement('ul')
 
- let girlsMenuTab=document.createElement('li')
- girlsMenuTab.textContent='Girls'
- let guysMenuTab=document.createElement('li')
- guysMenuTab.textContent='Guys'
- let saleMenuTab=document.createElement('li')
- saleMenuTab.textContent='Sales'
- 
- leftUlEl.append(girlsMenuTab, guysMenuTab, saleMenuTab)
- leftNavEl.append(leftUlEl)
- leftHeaderEl.append( leftNavEl)
+    let girlsMenuTab = document.createElement('li')
+    girlsMenuTab.textContent = 'Girls'
+    let guysMenuTab = document.createElement('li')
+    guysMenuTab.textContent = 'Guys'
+    let saleMenuTab = document.createElement('li')
+    saleMenuTab.textContent = 'Sales'
 
- let rightHeaderEl=document.createElement('div')
- rightHeaderEl.className='right-header'
+    leftUlEl.append(girlsMenuTab, guysMenuTab, saleMenuTab)
+    leftNavEl.append(leftUlEl)
+    leftHeaderEl.append(leftNavEl)
 
- let rightNavEl=document.createElement('nav')
- let rightUlEl=document.createElement('ul')
+    let rightHeaderEl = document.createElement('div')
+    rightHeaderEl.className = 'right-header'
 
- let searchLiEl=document.createElement('li')
- searchLiEl.textContent='🔍'
- let userLiEl=document.createElement('li')
- userLiEl.textContent='👤'
- let shoppingCartLiEl=document.createElement('li')
- shoppingCartLiEl.textContent='🛍️'
+    let rightNavEl = document.createElement('nav')
+    let rightUlEl = document.createElement('ul')
 
- rightUlEl.append(searchLiEl, userLiEl, shoppingCartLiEl)
- rightNavEl.append(rightUlEl)
- rightHeaderEl.append( rightNavEl)
- headerEl.append( leftHeaderEl, rightHeaderEl)
+    let searchLiEl = document.createElement('li')
+    searchLiEl.textContent = '🔍'
+    let userLiEl = document.createElement('li')
+    userLiEl.textContent = '👤'
+    let shoppingCartLiEl = document.createElement('li')
+    shoppingCartLiEl.textContent = '🛍️'
+
+    rightUlEl.append(searchLiEl, userLiEl, shoppingCartLiEl)
+    rightNavEl.append(rightUlEl)
+    rightHeaderEl.append(rightNavEl)
+    headerEl.append(leftHeaderEl, rightHeaderEl)
 }
 
-function renderMain(){
-    let mainEl=document.querySelector('main')
+function renderItem() {
+    let mainEl = document.querySelector('main')
     if (mainEl === null) return
-    mainEl.textContent=''
-    
-//    <h3 class="main-title">Home</h3>
-//      <div class="products">
-//        <div>
-//          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
-//          <h4>Name of product</h4>
-//          <span>Old price</span>
-//          <span>Current Price</span>
-//        </div>
-//      </div>
+    mainEl.textContent = ''
 
-let mainTitleEl=document.createElement('h3')
-mainTitleEl.textContent='Home'
+    for(let item of state.selectedItem){ 
 
-let productDisplayEL=document.createElement('div')
-productDisplayEL.className='products'
+    let selectedItem = document.createElement('div')
 
-for(let item of state.items){
+    let imgDivEl = document.createElement('div')
 
-let productEl=document.createElement('div')
-let productImgEl=document.createElement('img')
-productImgEl.src=item.image
-let productName=document.createElement('h4')
-productName.textContent=item.name
+    let itemImg = document.createElement('img')
+    itemImg.src = item.image
+    itemImg.className = 'selected-item-image'
 
-let oldPriceEl=document.createElement('span')
-oldPriceEl.textContent=`£${item.price}`
+    imgDivEl.append(itemImg)
 
+    let itemDetailsEl = document.createElement('div')
 
-let currentPriceEl=document.createElement('span')
-currentPriceEl.className='span-discount'
-if(item.discountedPrice===undefined){
-    currentPriceEl.textContent=''
-}
-else { 
-currentPriceEl.textContent=`£${item.discountedPrice}`
-oldPriceEl.className='old-price'
+    let itemNameEl = document.createElement('h4')
+    itemNameEl.textContent = item.name
+
+    let addToCartbtn = document.createElement('button')
+    addToCartbtn.textContent = 'Add to Cart'
+
+    itemDetailsEl.append(itemNameEl, addToCartbtn)
+    selectedItem.append(imgDivEl, itemDetailsEl)
+    mainEl.append(selectedItem)
+
+    }
+
 }
 
+function renderMain() {
+    let mainEl = document.querySelector('main')
+    if (mainEl === null) return
+    mainEl.textContent = ''
 
-productEl.append(productImgEl, productName, oldPriceEl, currentPriceEl)
-productDisplayEL.append(productEl)
-mainEl.append(mainTitleEl, productDisplayEL)
+    //    <h3 class="main-title">Home</h3>
+    //      <div class="products">
+    //        <div>
+    //          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+    //          <h4>Name of product</h4>
+    //          <span>Old price</span>
+    //          <span>Current Price</span>
+    //        </div>
+    //      </div>
+
+    let mainTitleEl = document.createElement('h3')
+    mainTitleEl.textContent = 'Home'
+
+    let productDisplayEL = document.createElement('div')
+    productDisplayEL.className = 'products'
+
+    for (let item of state.items) {
+
+        let productEl = document.createElement('div')
+        let productImgEl = document.createElement('img')
+        productImgEl.src = item.image
+        productImgEl.addEventListener('click', function(){
+            state.selectedItem.push(item)
+            console.log(state.selectedItem)
+            renderItem()
+        })
+        let productName = document.createElement('h4')
+        productName.textContent = item.name
+
+        let oldPriceEl = document.createElement('span')
+        oldPriceEl.textContent = `£${item.price}`
+
+
+        let currentPriceEl = document.createElement('span')
+        currentPriceEl.className = 'span-discount'
+        if (item.discountedPrice === undefined) {
+            currentPriceEl.textContent = ''
+        }
+        else {
+            currentPriceEl.textContent = `£${item.discountedPrice}`
+            oldPriceEl.className = 'old-price'
+        }
+
+
+        productEl.append(productImgEl, productName, oldPriceEl, currentPriceEl)
+        productDisplayEL.append(productEl)
+        mainEl.append(mainTitleEl, productDisplayEL)
+    }
 }
-}
-function render(){
+
+
+function render() {
     renderHeader()
     renderMain()
 }
